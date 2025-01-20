60bb59f8977ebd95c4d2eb42a90d35f89904b402
yjain3010 <yjain301000@gmail.com>
2025-01-20 17:40:37 +0530

    upds

--- src/Components/LandingPage/HeroPageSection4.jsx
+++ src/Components/LandingPage/HeroPageSection4.jsx
@@ -300,7 +300,7 @@ const HeroPageSection4 = ({ onComplete }) => {
               display: "flex",
               justifyContent: "space-between",
               width: "30%",
-              mt: isMobile ? 25 : 32,
+              mt: isMobile ? 0 : 32,
             }}
           >
             <Box
