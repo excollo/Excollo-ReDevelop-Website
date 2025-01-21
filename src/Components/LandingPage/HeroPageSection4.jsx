055a718ac4c38eb7719b15354b0c43ed2c2d828e
yjain3010 <yjain301000@gmail.com>
2025-01-21 12:14:37 +0530

    updated-code

--- src/Components/LandingPage/HeroPageSection4.jsx
+++ src/Components/LandingPage/HeroPageSection4.jsx
@@ -270,7 +270,7 @@ const HeroPageSection4 = ({ onComplete }) => {
             pt: "300px", // Specific padding for 321-374px
           },
           "@media (min-width: 375px) and (max-width: 392px)": {
-            pt: "280px", // Specific padding for 321-374px
+            pt: "320px", 
           },
           "@media (min-width: 393px) and (max-width: 599px)": {
             pt: "180px", // Maintain consistent padding for larger mobile screens
